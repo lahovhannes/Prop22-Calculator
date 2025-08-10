6abea4a9826420b2163815bab7eac23253774d39
lahovhannes <hovogrigoryan@gmail.com>
2025-08-09 21:50:06 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { optimizePackageImports: ['react', 'react-dom'] }
 };
 export default nextConfig;
