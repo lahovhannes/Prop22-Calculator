71e44465316adf7a877467bc10d99be93566ccf3
lahovhannes <hovogrigoryan@gmail.com>
2025-08-09 21:59:58 -0700

    Delete app/(site) directory

--- app/(site)/page.tsx
+++
@@ -1,5 +0,0 @@
-export const dynamic = 'force-dynamic';
-import Prop22Calculator from '@/components/Prop22Calculator'
-export default function Page() {
-  return <main><Prop22Calculator /></main>
-}
