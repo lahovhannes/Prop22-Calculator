da38b0de5192431503baaa0ac72b87420aeec010
lahovhannes <hovogrigoryan@gmail.com>
2025-08-09 21:59:00 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,11 @@
-export { default } from './(site)/page'
+export const dynamic = 'force-dynamic';
+
+import Prop22Calculator from '@/components/Prop22Calculator';
+
+export default function Page() {
+  return (
+    <main>
+      <Prop22Calculator />
+    </main>
+  );
+}
